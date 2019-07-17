cc222de9488c3d63c6aa50809bd02dfdb22248b1
Alexandra Kung <ak697@cornell.edu>
2019-07-17 11:21:09 -0500

    non recursive bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,35 @@
+// function bubbleSort(array) {
+
+//   let didSwap = true
+//   while (didSwap) {
+//     didSwap = false;
+//     let swapCount = 0
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] > array[i + 1]) {
+//         didSwap = true
+//         swapCount += 1;
+//         [array[i], array[i + 1]] = [array[i + 1], array[i]];
+//       }
+//     }
+//     if (!didSwap) {
+//       return array
+//     }
+//   }
+//   return array;
+// }
+
+
 function bubbleSort(array) {
+  if (array.length <= 1) {
+    return array
+  }
   for (let i = 0; i < array.length; i++) {
     if (array[i] > array[i + 1]) {
       [array[i], array[i + 1]] = [array[i + 1], array[i]];
     }
-    console.log(typeof array);
   }
-  return array;
+  console.log(array.splice(-1, 1))
+  //bubbleSort(array.splice(-1, 1))
+
+
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,13 +1,16 @@
-describe('Bubble Sort', function() {
-  it('should take an array as a parameter and return an array', function() {
+describe('Bubble Sort', function () {
+  it('should take an array as a parameter and return an array', function () {
     expect(typeof bubbleSort([])).toBe('object');
   });
 
-  it('handles an empty array', function() {
+  it('handles an empty array', function () {
     expect(bubbleSort([])).toEqual([]);
   });
 
-  it('handles a small arra of 3 items', function() {
+  it('handles a small arra of 3 items', function () {
     expect(bubbleSort([2, 1, 3])).toEqual([1, 2, 3]);
   });
+  it('handles an array that needs more than 1 pass', function () {
+    expect(bubbleSort([4, 0, 5, 2])).toEqual([0, 2, 4, 5]);
+  })
 });
