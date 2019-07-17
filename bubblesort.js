e2e3c785f77f7f3f51270b3d997ea360bf69a5ec
Madison Carr <madison.carr@utexas.edu>
2019-07-17 11:45:15 -0500

    works on recursive bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -18,18 +18,22 @@
 //   return array;
 // }
 
+function swap(thing1, thing2) {
+  if (thing1 > thing2) {
+    [thing1, thing2] = [thing2, thing1];
+  }
+}
 
-function bubbleSort(array) {
+function bubbleSort(array, lengthOfArray) {
   if (array.length <= 1) {
-    return array
+    return array;
   }
   for (let i = 0; i < array.length; i++) {
-    if (array[i] > array[i + 1]) {
-      [array[i], array[i + 1]] = [array[i + 1], array[i]];
-    }
+    swap(array[i], array[i + 1]);
   }
-  console.log(array.splice(-1, 1))
-  //bubbleSort(array.splice(-1, 1))
-
-
+  // console.log(array.splice(-1, 1));
+  // console.log(array);
+  // //bubbleSort(array.splice(-1, 1))
+  console.log(array);
+  return array;
 }
